4a0b5e44dcbf0b13961a6c21f2c3752bd5d26b36
Lucio Araujo <contatolucioaraujo@gmail.com>
2024-03-18 22:28:17 -0300

    start the project

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type"],
+    })
+)
+
+app.listen(PORT, () => console.log(`The server is running on port ${PORT}`))
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.use("api/auth", authRoutes)
